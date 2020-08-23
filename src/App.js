93c040718f713f2fd0ecc528d185fd6e1c295171
RitvijVejare <rutu9295@gmail.com>
2020-08-23 19:05:26 +0530

    started repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import {Switch, Grid, Typography, Button, Paper} from "@material-ui/core";
+import {ThemeProvider,createMuiTheme} from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        
+    );
 }
 
 export default App;
